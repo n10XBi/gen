50897d1cb25b99db6bd81198ca93e92ad10f846f
n10XBi <164579514+n10XBi@users.noreply.github.com>
2025-01-01 17:22:05 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 // Real Owner moved to db.data.datas.rowner
-global.mods = ['6282337245566', '6285392167868']
+global.mods = ['6285291181791', '6283891751100']
 global.APIs = { // API Prefix
 	// name: 'https://website'
 	nrtm: 'https://nurutomo.herokuapp.com',
